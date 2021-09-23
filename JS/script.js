802c2934836681ffaa4f56097eeeb298b1b5bfe7
Raquel Custodio DAvila <raquel.engflorestal@gmail.com>
2021-09-23 11:31:21 -0300

    initial commit

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+//transforma o texto em caixa alta
+function maiusculas() {
+    var x=document.getElementById("id_nome");
+    x.value=x.value.toUpperCase();
+}
+// Troca a cor no fundo do campo do formulário
+function trocaCorCampo() {
+    document.getElementById("id_mensagem").style["background-color"]="#e0deda";
+}
